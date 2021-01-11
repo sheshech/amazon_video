cc423233485ca0ab00d46a62817b655e6b7c4d9e
sheshech <sheshech@gmail.com>
2021-01-11 22:04:19 +0900

    use props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
+function Movie(props) {
+  return <h1>I like { props.fav }</h1>;
+}
 
 function App() {
   return (
   <div>
     <h1>test</h1>
+    <Movie fav="kimchi" />
+    <Movie fav="samgiopsal" />
+    <Movie fav="chukumi" />
   </div>
   );
 }
